f993bb06991fdddc1458296b644bb1e37e92a079
bijesh <bijesh.jayaprakash@gmail.com>
2024-11-14 11:37:33 +0530

    add card widget

--- app/src/main/java/com/bijesh/udemy/compose/MainActivity.kt
+++ app/src/main/java/com/bijesh/udemy/compose/MainActivity.kt
@@ -7,14 +7,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bijesh.udemy.compose.ui.theme.Udemy_composeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,23 +32,36 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Udemy_composeTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    CreateBizCard()
-                }
-
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                Surface(color = MaterialTheme.colorScheme.background) {
+//                    CreateBizCard()
 //                }
+                    CreateBizCard(modifier = Modifier.padding(innerPadding))
+                }
             }
         }
     }
 }
 
 @Composable
-fun CreateBizCard(){
-    Surface(modifier = Modifier
-        .fillMaxWidth().fillMaxHeight()) {
+fun CreateBizCard(modifier: Modifier) {
+    Surface(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(modifier = Modifier
+            .width(200.dp)
+            .height(390.dp)
+            .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = CardDefaults.cardElevation(),
+            colors = androidx.compose.material3.CardDefaults.cardColors(),
+            onClick = { /*TODO*/ }
 
+        ) {
 
+        }
     }
 }
 
@@ -55,6 +77,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Udemy_composeTheme {
-        CreateBizCard()
+        CreateBizCard(modifier = Modifier)
     }
 }
